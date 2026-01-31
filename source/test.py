777396c39c8792ccc8e6a4f06cf15b75911d544a
chrisling0001 <chrisling040222@gmail.com>
2026-01-31 20:56:02 +0800

    something unrelated was changed

--- source/test.py
+++ source/test.py
@@ -3,19 +3,4 @@ from operator import itemgetter
 from collections import namedtuple
 import os
 
-files = os.listdir('F:/Pythonn/PythonCode/demo')
-if any(name.endswith('.py') for name in files):
-    print('There be python!')
-else:
-    print('Sorry, no python.')
-# Output a tuple as CSV
-s = ('ACME', 50, 123.45)
-print(','.join(str(x) for x in s))
-# Data reduction across fields of a data structure
-portfolio = [
-{'name':'GOOG', 'shares': 50},
-{'name':'YHOO', 'shares': 75},
-{'name':'AOL', 'shares': 20},
-{'name':'SCOX', 'shares': 65}
-]
-min_shares = min(s['shares'] for s in portfolio)
+
