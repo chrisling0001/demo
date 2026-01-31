cabca38f86e98b49af8a01b4c8a652d78d60f2fc
chrisling0001 <chrisling040222@gmail.com>
2026-01-31 20:53:02 +0800

    Nothing useful was changed

--- source/demo.py
+++ source/demo.py
@@ -1,3 +1,6 @@
+from operator import attrgetter
+
+
 class Person():
     blood_color = "red"
     skin = "yellow"
@@ -9,3 +12,16 @@ class Person():
     def greet(self):
         print("Hello, " + self.name.title() + " you're "
                  + str(self.age) + " years old.")
+
+class User():
+    def __init__(self, user_id):
+        self.user_id = user_id
+    
+    def __repr__(self):
+        return 'User({})'.format(self.user_id)
+
+def sort_notcompare():
+    users = [User(23), User(3), User(99)]
+    print(users)
+    print(sorted(users, key=lambda u:u.user_id))
+
