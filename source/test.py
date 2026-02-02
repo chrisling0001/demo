57c4f772465b9cc72f02598c42db4968a1eb1876
chrisling0001 <chrisling040222@gmail.com>
2026-02-02 22:05:17 +0800

    Nothing important was changed

--- source/test.py
+++ source/test.py
@@ -1,6 +1,14 @@
-from itertools import groupby, compress
-from operator import itemgetter
-from collections import namedtuple
-import os
+import re, os, re
+import unicodedata
+from urllib.request import urlopen
+from fnmatch import fnmatch, fnmatchcase
 
+s1 = 'Spicy Jalape\u00f1o'
+s2 = 'Spicy Jalapen\u0303o'
+t1 = unicodedata.normalize('NFD', s1)
+t2 = unicodedata.normalize('NFC', s2)
 
+t3 = unicodedata.normalize('NFD', s1)
+t4 = unicodedata.normalize('NFD', s2)
+# print(t3 == t4)
+print(''.join(c for c in t1 if not unicodedata.combining(c)))
