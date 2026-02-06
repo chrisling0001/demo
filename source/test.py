dc148a5f445b3278b47b246020388604f61c7a18
chrisling0001 <chrisling040222@gmail.com>
2026-02-06 20:53:56 +0800

    Nothing important was changed.

--- source/test.py
+++ source/test.py
@@ -1,14 +1,38 @@
-import re, os, re
-import unicodedata
-from urllib.request import urlopen
-from fnmatch import fnmatch, fnmatchcase
+from collections import defaultdict, Counter, deque, namedtuple
+from enum import Enum
+""" def a_new_decorator(a_func):
+    def wrap_the_function():
+        print("I'm doing some boring work b4 executing a_func()")
+        a_func()
+        print("I'm doing some boring work after executing a_func()")
+    return wrap_the_function
 
-s1 = 'Spicy Jalape\u00f1o'
-s2 = 'Spicy Jalapen\u0303o'
-t1 = unicodedata.normalize('NFD', s1)
-t2 = unicodedata.normalize('NFC', s2)
+def a_function_requring_decoration():
+    print("I'm the function which needs some decoration" \
+            " to remove my foul smell.")
 
-t3 = unicodedata.normalize('NFD', s1)
-t4 = unicodedata.normalize('NFD', s2)
-# print(t3 == t4)
-print(''.join(c for c in t1 if not unicodedata.combining(c)))
+@a_new_decorator
+def a_function_requring_decoration():
+    # Hey you! Decorate me!
+    print("I'm the function which needs some decoration" \
+            " to remove my foul smell") """
+
+class Species(Enum):
+    cat = 1
+    dog = 2
+    horse = 3
+    aardvark = 4
+    butterfly = 5
+    owl = 6
+    platypus = 7
+    dragon = 8
+    unicorn = 9
+
+    kitten = 1 
+    puppy = 2 
+
+Animal = namedtuple('Animal', 'name age type')
+perry = Animal(name="perry", age=22, type=Species.cat)
+dragon = Animal(name='dragon', age=92, type=Species.dragon)
+print(perry)
+print(dragon)
